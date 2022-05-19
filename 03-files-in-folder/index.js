6d884e7b0e343be4efbd9cf6c20b5f53198d9d9d
Igor Shaymukhametov <shaym@yandex.ru>
2022-05-19 16:10:36 +0500

    feat: done 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,3 +1,16 @@
-const readdir = require('fs').promises.readdir;
-const path = require('path').join(__dirname, 'secret-folder');
-console.log(readdir(path));
+const { readdir } = require('fs/promises');
+const { stat } = require('fs/promises');
+const path = require('path');
+const dirPath = path.join(__dirname, 'secret-folder');
+
+
+(async function () {
+  (await readdir(dirPath, {withFileTypes: true}))
+    .filter(el => !el.isDirectory())
+    .forEach(async (el) => {
+      const { name } = el;
+      const filePath = path.join(dirPath, name);
+      const { size } = await stat(filePath);
+      console.log( `${name.split('.')[0]} - ${name.split('.')[1]} - ${(size)} bytes`)
+    })
+})();
