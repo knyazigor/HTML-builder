1cb39906030523c7fae4a1408f14ffa956534fc4
Igor Shaymukhametov <shaym@yandex.ru>
2022-05-23 14:50:47 +0500

    refactor: add greetings to 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,6 +6,9 @@ const process = require('node:process');
 const path = require('path').join(__dirname, 'text.txt');
 
 (async function () {
+  const greetings = '\nWake up, Neo...\nThe Matrix has you...\nFollow the White Rabbit\n\n';
+  console.log(greetings);
+  
   const stream = fs.createWriteStream(path, 'utf-8');
   const rl = readLine.createInterface({ input: process.stdin, output: process.stdout, prompt: 'Your answer: ' });
   rl.on('close', () => {
