2bc7fcda3163db52c2ab9da413385ea773c56867
Igor Shaymukhametov <shaym@yandex.ru>
2022-05-23 14:57:24 +0500

    feat: add first line check 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,7 +8,7 @@ const path = require('path').join(__dirname, 'text.txt');
 (async function () {
   const greetings = '\nWake up, Neo...\nThe Matrix has you...\nFollow the White Rabbit\n\n';
   console.log(greetings);
-  
+
   const stream = fs.createWriteStream(path, 'utf-8');
   const rl = readLine.createInterface({ input: process.stdin, output: process.stdout, prompt: 'Your answer: ' });
   rl.on('close', () => {
@@ -17,14 +17,16 @@ const path = require('path').join(__dirname, 'text.txt');
     process.exit(0);
   }) 
   rl.prompt();
+  let firstLine = true;
   for await (const line of rl) {
     switch (line.trim()) {
       case 'exit':
         rl.close();
         break;
       default:        
-        stream.write(line+'\n');
+        stream.write((firstLine ? '' : '\n') + line);
         rl.prompt();
+        if (firstLine) firstLine = false;
     }
   }
 }
