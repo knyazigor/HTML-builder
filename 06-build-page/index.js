c0f59bfcef5d1ac17018cd20aae7e1b1852fd688
Igor Shaymukhametov <shaym@yandex.ru>
2022-05-23 15:21:38 +0500

    refactor: small code style improvements

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,32 +5,32 @@ const path = require('path');
 (async function () {
   try {
     //make directory
-  const destDirPath = path.join(__dirname, 'project-dist');  
-  await rm(destDirPath, {recursive: true, force: true});
-  await mkdir(destDirPath, {recursive: true});
-  //insert components
-  const componentsPath = path.join(__dirname, 'components')  
-  const components = (await readdir(componentsPath, {withFileTypes: true}))
-    .filter(el => !el.isDirectory() && path.extname(el.name).toLowerCase() === '.html')
-    .map(el => el.name);
-  let template = await readFile(path.join(__dirname, 'template.html'), 'utf-8');
-  for (const component of components) {
-    const re = new RegExp(`{{${component.replace(/\.[^/.]+$/, '')}}}`, 'g');
-    const data = await readFile(path.join(componentsPath, component), 'utf-8');
-    template = template.replace(re, data);    
-  }
-  await writeFile(path.join(destDirPath, 'index.html'), template, 'utf-8');
-  //merge styles
-  const stylesPath = path.join(__dirname, 'styles');
-  const styles = (await readdir(stylesPath, {withFileTypes: true}));
-  const stylesBundle = (await Promise.all(
-      styles.map(el => readFile(path.join(stylesPath, el.name), 'utf-8'
-    )))).reduce((acc, el) => acc + el, '');
-  await writeFile(path.join(destDirPath, 'style.css'), stylesBundle, 'utf-8');
-  //copy assets 
-  const asssetsDest = path.join(destDirPath, 'assets');
-  const assetsSrc = path.join(__dirname, 'assets');
-  await copyDir(assetsSrc, asssetsDest);
+    const destDirPath = path.join(__dirname, 'project-dist');  
+    await rm(destDirPath, {recursive: true, force: true});
+    await mkdir(destDirPath, {recursive: true});
+    //insert components
+    const componentsPath = path.join(__dirname, 'components')  
+    const components = (await readdir(componentsPath, {withFileTypes: true}))
+      .filter(el => !el.isDirectory() && path.extname(el.name).toLowerCase() === '.html')
+      .map(el => el.name);
+    let template = await readFile(path.join(__dirname, 'template.html'), 'utf-8');
+    for (const component of components) {
+      const re = new RegExp(`{{${component.replace(/\.[^/.]+$/, '')}}}`, 'g');
+      const data = await readFile(path.join(componentsPath, component), 'utf-8');
+      template = template.replace(re, data);    
+    }
+    await writeFile(path.join(destDirPath, 'index.html'), template, 'utf-8');
+    //merge styles
+    const stylesPath = path.join(__dirname, 'styles');
+    const styles = (await readdir(stylesPath, {withFileTypes: true}));
+    const stylesBundle = (await Promise.all(
+        styles.map(el => readFile(path.join(stylesPath, el.name), 'utf-8'
+      )))).reverse().reduce((acc, el) => acc + el, '');
+    await writeFile(path.join(destDirPath, 'style.css'), stylesBundle, 'utf-8');
+    //copy assets 
+    const asssetsDest = path.join(destDirPath, 'assets');
+    const assetsSrc = path.join(__dirname, 'assets');
+    await copyDir(assetsSrc, asssetsDest);
   }
   catch (err) {
     console.log(err);
