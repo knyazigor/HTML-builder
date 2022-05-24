2343442fe9aeb3f3a94f2d437ecf6dbd23c791e3
Igor Shaymukhametov <shaym@yandex.ru>
2022-05-24 23:28:44 +0500

    refactor: add line breaking for 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,7 +7,7 @@ const path = require('path');
     const data = (await Promise.all((await readdir(stylesPath, {withFileTypes: true}))
         .filter(el => !el.isDirectory() && path.extname(el.name) === '.css')
         .map(el => readFile(path.join(stylesPath, el.name), 'utf-8'))))
-      .reduce((acc, el) => acc + el, '');
+      .reduce((acc, el) => acc + el + '\n', '');
 
     const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
     await writeFile(bundlePath, data, 'utf-8');
